cbc4a366370f98c6c03add9f23b0a83afdb50619
ravi chandola <ravichandola94@gmail.com>
2020-03-18 05:05:36 -0700

    Routing demo

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const http = require('http');
 const port = 8084;
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+    //console.log(req.url,req.method,req.headers);
+
+    const url = req.url;
+
+    if(url === '/'){
+         //setting the headers
+    res.setHeader('Content-Type','text/html');
+
+    // Write in a response we need to show in a browser
+
+    res.write('<html>');
+    res.write('<head><title>Enter Your Message</title></head> ');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="ravi"><button type="submit">Submit</button></form></body>');
+    res.write('</html>');
+
+    //End of response
+
+    return res.end();
+     
+    }
 
     //setting the headers
     res.setHeader('Content-Type','text/html');
@@ -15,7 +34,7 @@ const server = http.createServer((req,res)=>{
     res.write('</html>');
 
     //End of response
-    
+
     res.end();
 
     // After the end of response if we want to write anything , it will throw an error; 
