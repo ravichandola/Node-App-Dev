da80f8594034d2eba19f59cd5a6c286c125ba6a1
ravi chandola <ravichandola94@gmail.com>
2020-03-20 01:31:06 -0700

    Parsing request body

--- app.js
+++ app.js
@@ -1,46 +1,39 @@
 const http = require('http');
+const fs = require('fs');
+
 const port = 8084;
 
-const server = http.createServer((req,res)=>{
-    //console.log(req.url,req.method,req.headers);
-
-    const url = req.url;
-
-    if(url === '/'){
-         //setting the headers
-    res.setHeader('Content-Type','text/html');
-
-    // Write in a response we need to show in a browser
-
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>Enter Your Message</title></head> ');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="ravi"><button type="submit">Submit</button></form></body>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
-
-    //End of response
-
     return res.end();
-     
-    }
-
-    //setting the headers
-    res.setHeader('Content-Type','text/html');
-
-    // Write in a response we need to show in a browser
-
-    res.write('<html>');
-    res.write('<head><title>Home Page</title></head> ');
-    res.write('<body><h1>Welcome to HomePage</h1></body>');
-    res.write('</html>');
-
-    //End of response
-
-    res.end();
-
-    // After the end of response if we want to write anything , it will throw an error; 
-    // res.write(); 
-
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(port);
-
