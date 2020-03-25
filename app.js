e2951c0239fde173b690686f38cda655a34b5a96
ravi chandola <ravichandola94@gmail.com>
2020-03-25 16:06:56 -0700

    Modify 404 page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // Access the static public folder
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
