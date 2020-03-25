0bfe97a41ce05cdd68bfa95697420b2fd024d04f
ravi chandola <ravichandola94@gmail.com>
2020-03-24 23:15:28 -0700

    Make utility file

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express  = require('express');
-
+const path = require('path');
 const router = express.Router();
+const rootDir = require('../util/path');
 
 router.get('/add-product',(req, res, next) => {
     console.log('In middleware 1');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product </button></form>'); //Send the body
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
    // Allows the request to continue to the next middleware in line
    
   });
