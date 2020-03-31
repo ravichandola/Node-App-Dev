a98f05b9a6eaa89c50d73f934dd0329f4e9aa1fc
ravi chandola <ravichandola94@gmail.com>
2020-03-31 09:49:52 -0700

    Added Product id functionality successfully

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,49 +1,53 @@
 const Product = require('../models/product');
 
-
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: 'All Products',
+      path: '/products'
     });
   });
 };
 
-exports.getIndex = (req,res,next) => {
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      path: '/'
     });
   });
-}
+};
 
-exports.getCart = (req,res,next) => {
-  res.render('shop/cart',{
-    path : '/cart',
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
     pageTitle: 'Your Cart'
   });
 };
 
-exports.getOrders = (req,res,next) => {
-  res.render('shop/orders',{
-    path : '/orders',
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
     pageTitle: 'Your Orders'
   });
 };
 
-exports.getCheckout = (req,res,next) =>{
-  res.render('shop/checkout',{
-    path : '/checkout',
-    pageTitle : 'Checkout'
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
   });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -8,13 +8,14 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 
-router.get('/products',shopController.getProducts);
+router.get('/products', shopController.getProducts);
 
-router.get('/cart',shopController.getCart);
+router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart',shopController.getOrders);
+router.get('/cart', shopController.getCart);
 
-router.get('/checkout',shopController.getCheckout);
+router.get('/orders', shopController.getOrders);
 
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
